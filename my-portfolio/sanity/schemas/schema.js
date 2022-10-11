4b1aa0f5f4eb57f3682ac5bd9d02dbf279597eac
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-11 14:48:38 +0300

    project added

--- my-portfolio/sanity/schemas/schema.js
+++ my-portfolio/sanity/schemas/schema.js
@@ -1,11 +1,15 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-
+import schemaTypes from "all:part:@sanity/base/schema-type";
+import pageInfo from "./pageInfo";
+import project from "./project";
+import experience from "./experience";
+import skill from "./skill";
+import social from "./social";
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([]),
-})
+  name: "default",
+  types: schemaTypes.concat([pageInfo, skill, experience, project, social]),
+});
